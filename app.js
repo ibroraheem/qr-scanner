0a6ef71d74474ba70d99ff3116d15a9ae4db4f19
Ibrahim Abdulraheem <ibroraheem95@gmail.com>
2022-06-02 10:29:22 +0100

    Edit

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const wrapper = document.querySelector(".wrapper"),
-form = document.querySelector("form"),
-fileInp = form.querySelector("input"),
-infoText = form.querySelector("p"),
-closeBtn = document.querySelector(".close"),
-copyBtn = document.querySelector(".copy");
+    form = document.querySelector("form"),
+    fileInp = form.querySelector("input"),
+    infoText = form.querySelector("p"),
+    closeBtn = document.querySelector(".close"),
+    copyBtn = document.querySelector(".copy");
 
 function fetchRequest(file, formData) {
     infoText.innerText = "Scanning QR Code...";
@@ -12,7 +12,7 @@ function fetchRequest(file, formData) {
     }).then(res => res.json()).then(result => {
         result = result[0].symbol[0].data;
         infoText.innerText = result ? "Upload QR Code to Scan" : "Couldn't scan QR Code";
-        if(!result) return;
+        if (!result) return;
         document.querySelector("textarea").innerText = result;
         form.querySelector("img").src = URL.createObjectURL(file);
         wrapper.classList.add("active");
@@ -23,7 +23,7 @@ function fetchRequest(file, formData) {
 
 fileInp.addEventListener("change", async e => {
     let file = e.target.files[0];
-    if(!file) return;
+    if (!file) return;
     let formData = new FormData();
     formData.append('file', file);
     fetchRequest(file, formData);
